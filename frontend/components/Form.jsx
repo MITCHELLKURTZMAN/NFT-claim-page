0f214726ce28f1401c560e4c7829b89543eec6bc
Kjypp Padua <kjypprpadua@gmail.com>
2023-04-26 11:38:20 +0900

    small design changes

--- frontend/components/Form.jsx
+++ frontend/components/Form.jsx
@@ -26,48 +26,51 @@ const Form = ({
                         <div class="w-full p-20">
                             <h1 class="text-5xl font-bold mb-5"> Claim your <img src={kinicLogo} class="mb-2 h-8 inline-flex" /> Tokens </h1>
                             
-                            <label class="text-blue-800 text-lg uppercase font-black">What to do</label>
-                            <p class="block text-sm font-medium text-gray-900 mb-2"> 
-                                <span class="font-black text-gray-700">1.</span> &nbsp;
-                                Your wallet account number 
-                                {subAccountID === "" ? ' and ' : ', sub-account ID, and '}
-                                principal ID are displayed in the blue box below. 
-                                Click the checkbox to confirm that they are correct.
-                            </p>
-                            <p class="block text-sm font-medium text-gray-900"> 
-                                <span class="font-black text-gray-700">2.</span> &nbsp;
-                                Enter your Internet Identity Principal ID and click Submit!
-                            </p>
-                            <span onClick={() => setOpenModal(true)}
-                                class="block text-xs font-semibold text-blue-600 hover:underline cursor-pointer"> 
-                                How do I find my Internet Identity Principal ID?
-                            </span>
+                            <label class="text-lg uppercase font-black">What to do</label>
 
-                            <div class="my-5 text-sm bg-blue-100 p-6 rounded-lg">
+                            <ol class="list-decimal ml-5 font-black text-sm text-gray-700">
+                                <li>
+                                    <p class="block text-sm font-medium text-gray-900 mb-2"> 
+                                        Your wallet account number 
+                                        {subAccountID === "" ? ' and ' : ', sub-account ID, and '}
+                                        principal ID are displayed in the blue box below. 
+                                        Click the checkbox to confirm that they are correct.
+                                    </p>
+                                </li>
+                                <li>
+                                <p class="block text-sm font-medium text-gray-900"> 
+                                    Enter your Internet Identity Principal ID and click Submit!
+                                </p>
+                                <span onClick={() => setOpenModal(true)}
+                                    class="block text-xs font-semibold text-blue-600 hover:underline cursor-pointer"> 
+                                    How do I find my Internet Identity Principal ID?
+                                </span>
+                                </li>
+                            </ol>
+
+                            <div class="mt-5 text-sm bg-blue-100 p-6 rounded-lg">
                                 <p class="block uppercase font-bold"> Account Number</p>
-                                <p class="block mb-3 font-medium">{accountNbr}</p>
-
+                                <p class="block mb-3 font-medium text-gray-600">{accountNbr}</p>
                                 {subAccountID === "" ? '' : 
                                     <div>
                                         <p class="block uppercase font-bold"> Sub-Account ID</p>
-                                        <p class="block mb-3 font-medium">{subAccountID}</p>
+                                        <p class="block mb-3 font-medium text-gray-600">{subAccountID}</p>
                                     </div>
                                 }
-
                                 <p class="block uppercase font-bold"> Principal ID</p>
-                                <p class="block font-medium">{principalID}</p>
-
-                                <span class="block mt-5 text-xs font-base"> 
-                                    If the above is incorrect, please contact us on Twitter:&nbsp;
-                                    <a href="https://twitter.com/kinic_app" target="blank" 
-                                        class="font-semibold text-blue-600 hover:underline cursor-pointer">
-                                        https://twitter.com/kinic_app
-                                    </a>
-                                </span>
+                                <p class="block font-medium text-gray-600">{principalID}</p>
                             </div>
 
+                            <span class="block mt-1 mb-5 text-xs font-base"> 
+                                If the above is incorrect, please contact us on Twitter:&nbsp;
+                                <a href="https://twitter.com/kinic_app" target="blank" 
+                                    class="font-semibold text-blue-600 hover:underline cursor-pointer">
+                                    https://twitter.com/kinic_app
+                                </a>
+                            </span>
+
                             <fieldset>
-                                <div class="flex items-center items-start mb-5">
+                                <div class="flex items-center items-start mb-3.5">
                                     <input 
                                         id="confirm" 
                                         type="checkbox" 
@@ -111,7 +114,7 @@ const Form = ({
             </div>
             <Modal trigger={openModal} setTrigger={setOpenModal}>
                 <div class="text-md">
-                    <h1 class="text-blue-800 uppercase text-lg font-black mb-3"> How to find your Internet Identity Principal ID </h1>
+                    <h1 class="uppercase text-lg font-black mb-3"> How to find your Internet Identity Principal ID </h1>
 
                     <p class="block font-medium text-gray-900 my-3"> 
                         <span class="font-black text-gray-700">1.</span> Sign in to your NNS Account here: 
